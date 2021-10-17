91dea1dec9e4c841a8f2fa2403d03f93ee2aad3e
Rasmus Arras <rarras@tktk.ee>
2021-10-17 23:38:50 +0300

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti (cpu) ja kasutaja (user)
+            //mõlemad mängijad viskavad täringut
+            //programm kontrollib, kumb mängija viskas rohkem
+            //mängija, kes viskab rohkem, on mängu võitja
+            //*täringuid visatakse kolm korda
+            //programm kuulutab võitja
+
+            Random rnd = new Random();
+
+            
+
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for(int throwRound = 0;throwRound < 3; throwRound++)
+            {
+                int cpuRandom = rnd.Next(1, 7);
+                int userRandom = rnd.Next(1, 7);
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom})");
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja on viske võitnud.");
+                    userScore = userScore + 1;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti on viske võitnud.");
+                    cpuScore = cpuScore + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Vise jäi viiki!");
+                }
+            } 
+            if(userScore == 3 && cpuScore != 3)
+            {
+                Console.WriteLine("Kasutaja võitis mängu. Palju õnne!");
+            }
+            else
+            {
+                Console.WriteLine("Arvuti võitis mängu");
+            }
+        }
+    }
+}
---
+++ ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I will not skatevoard in the halls
+         
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i+1}. I will not skatevoard in the halls".ToUpper());
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
